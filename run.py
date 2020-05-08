d62c8b33568d00d485e401e0b7d4dc717572f5ef
Coaco <naseeihility@163.com>
2020-05-08 17:48:04 +0800

    bugfix

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from input.qa.run_squad import run_squad
 K = 4
 ROOT = './input/tweet-sentiment-extraction/'
 
+# TODO：train on 1-4, test on
 def run(cross_validation):
     if cross_validation:
         for i in range(1, K + 1):
@@ -12,7 +13,7 @@ def run(cross_validation):
             run_squad(train_file, predict_file)
     else:
         train_file = os.path.join(ROOT, "original/train.json")
-        predict_file = os.path.join(ROOT, "original/train.json")
+        predict_file = os.path.join(ROOT, "original/test.json")
         run_squad(train_file, predict_file)
 
 
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 python run.py --model_type roberta \
               --model_name_or_path roberta-base \
-              --do_train
+              --do_train \
+              --per_gpu_train_batch_size 8
